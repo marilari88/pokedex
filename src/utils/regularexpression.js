2e71907c00bc1e1cb160330fb292bb8d31f3fb65
Marco Ilari <marilari88@gmail.com>
2021-10-12 08:36:36 +0200

    Fix regularexpression escape function warning

--- src/utils/regularexpression.js
+++ src/utils/regularexpression.js
@@ -1,3 +1,4 @@
 export const escapeRegularExpression = (stringToGoIntoTheRegex) => {
-  return stringToGoIntoTheRegex.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+    return stringToGoIntoTheRegex.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
 };
+
