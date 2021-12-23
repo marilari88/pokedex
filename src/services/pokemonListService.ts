4beafae341e043d4d03987a7afd779e01705cd79
Marco Ilari <marilari88@gmail.com>
2021-12-23 11:10:34 +0100

    Improvements to TS refactoring

--- src/services/pokemonListService.ts
+++ src/services/pokemonListService.ts
@@ -4,11 +4,16 @@ import { PokemonItem } from "../interfaces/pokemonItem";
 // Consider only pokemon up to the VIII Generation
 const POKEMON_NUMBERS = 898;
 
+type AxiosResponse = {
+  results: Array<PokemonItem>;
+};
+
 const getPokemonList = async () => {
   const pokemonListString = localStorage.getItem("pokemonList");
-  if (pokemonListString != null) return JSON.parse(pokemonListString);
+  if (pokemonListString != null)
+    return JSON.parse(pokemonListString) as Array<PokemonItem>;
 
-  const response = await axios.get<{ results: Array<PokemonItem> }>(
+  const response = await axios.get<AxiosResponse>(
     `https://pokeapi.co/api/v2/pokemon/?limit=${POKEMON_NUMBERS}`
   );
 
