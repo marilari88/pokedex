3315c93df10eb26be8cadb560b6e93f5d8d7c749
Marco Ilari <marilari88@gmail.com>
2021-12-28 13:03:31 +0100

    🐛 Fix getPokemonCaughtArray returning type

--- src/services/catchService.ts
+++ src/services/catchService.ts
@@ -1,13 +1,11 @@
-import { PokemonItem } from "../interfaces/pokemonItem";
-
 // Get pokemon array that is already in localstorage
-const getPokemonCaughtArray = (): Array<PokemonItem> => {
+const getPokemonCaughtArray = (): string[] => {
   const lsArray = localStorage.getItem("pokemonCaughtArray");
   return lsArray ? JSON.parse(lsArray) : [];
 };
 
 // Save list of pokemon to the localStorage
-const savePokemonCaughtArray = (pokemonArray: Array<PokemonItem>) => {
+const savePokemonCaughtArray = (pokemonArray: string[]) => {
   localStorage.setItem("pokemonCaughtArray", JSON.stringify(pokemonArray));
 };
 
