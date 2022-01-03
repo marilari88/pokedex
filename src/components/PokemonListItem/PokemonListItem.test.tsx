89fb2fe02390b6e290d84752b4c7604189ea68ff
Marco Ilari <marilari88@gmail.com>
2022-01-03 11:58:28 +0100

    🏷️  Refactor PokemonListItem to TS (#13)

--- src/components/PokemonListItem/PokemonListItem.test.tsx
+++ src/components/PokemonListItem/PokemonListItem.test.tsx
@@ -2,13 +2,13 @@ import { screen, render } from "@testing-library/react";
 import PokemonListItem from "./PokemonListItem";
 
 it("Show pokemon name", () => {
-  const mockPokemon = { name: "ditto" };
+  const mockPokemon = { name: "ditto", url: "https:/fakeurl.com/ditto" };
   render(<PokemonListItem pokemon={mockPokemon} />);
   expect(screen.getByText(/ditto/i)).toBeInTheDocument();
 });
 
 it("Capitalized name pokemon", () => {
-  const mockPokemon = { name: "ditto" };
+  const mockPokemon = { name: "ditto", url: "https:/fakeurl.com/ditto" };
   render(<PokemonListItem pokemon={mockPokemon} />);
   expect(screen.getByText(/Ditto/)).toBeInTheDocument();
 });
